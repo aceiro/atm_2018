b2a952cdfb80f32542bf95954c3dd4b01ad5bf65
gismarb <gismar_barbosa@yahoo.com.br>
2018-10-24 20:57:13 -0300

    ATM7: Criação do Código main.c #10

---
+++ main.c
@@ -0,0 +1,26 @@
+/*
+ *
+ * Modulo Main (modulo central) do Programa
+ *
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ *
+ * #include <calc_media_valor_emp.h>
+ * #include <calc_media_valor_pag.h>
+ * #include <calc_media_valor_liq.h>
+ * #include <calc_media_agrup_ite_serv.h>
+ *
+ */
+
+int main()
+{
+    printf("\n\n\t\tEste sera o Modulo Central do Programa Sumarizador");
+    printf("\n\n\t\tTodos os Modulos serao chamados (entrada e saida por aqui)\n\n");
+
+
+    return 0;
+}
